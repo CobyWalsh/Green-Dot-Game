9286dcbb201f7732ae6941116b4a1574b782b666
Coby Walsh <cobywalsh7@yahoo.com>
2024-02-02 18:15:49 -0700

    added purple dot

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const canvas = document.getElementById('gameArea');
 const ctx = canvas.getContext("2d");
 
+let x = 100;
+let y = 100;
+let radius = 50;
+
+// Game Loop
+function drawGame(){
+  clearScrean();
+  drawGreenBlob();
+}
+
+function drawGreenBlob(){
+    ctx.fillStyle = "purple";
+    ctx.beginPath();
+    ctx.arc(x,y, radius,0, Math.PI * 2);
+    ctx.fill();
+}
+
+function clearScrean(){
 ctx.fillStyle = "orange";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+drawGame();
