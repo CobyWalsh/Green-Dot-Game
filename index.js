ff60789bbfbccbff7d4bef0a8426fdf9ea84087e
Coby Walsh <cobywalsh7@yahoo.com>
2024-02-03 11:47:20 -0700

    gave dot ability to move down

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const ctx = canvas.getContext("2d");
 let x = 100;
 let y = 100;
 let radius = 50;
+let downPressed = false;
 
 // Game Loop
 function drawGame(){
+  requestAnimationFrame(drawGame);  
   clearScrean();
+  inputs();
   drawGreenBlob();
 }
+
+ function inputs() {
+    if (downPressed) {
+        y = y + 10;
+    }
+ }
 
 function drawGreenBlob(){
     ctx.fillStyle = "purple";
@@ -22,5 +31,22 @@ function clearScrean(){
 ctx.fillStyle = "orange";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
+
+document.body.addEventListener("keydown", keyDown);
+document.body.addEventListener("keyup", keyup);
+
+function keyDown(event) {
+    // down
+    if (event.keyCode == 40) {
+        downPressed = true;
+    }
+}
+
+function keyup(event) {
+    if (event.keyCode == 40) {
+        downPressed = false;
+    }
+}
+
 
 drawGame();
